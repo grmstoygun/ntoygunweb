7767c154a480c803d77b643f25c6d7d5ecdd82e2
Toygun Gormus <grmstoygun@gmail.com>
2023-09-06 17:14:03 +0300

    Added Product data model.

--- ntoygun.DataAcess/Migrations/20230906135637_SeedProductTable.Designer.cs
+++ ntoygun.DataAcess/Migrations/20230906135637_SeedProductTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ntoygun.DataAcess.Data;
 
@@ -10,9 +11,11 @@ using ntoygun.DataAcess.Data;
 namespace ntoygun.DataAcess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230906135637_SeedProductTable")]
+    partial class SeedProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ntoygun.DataAcess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ntoygun.DataAcess.Data;
+using ntoygun.DataAcess.Repository.IRepository;
+using ntoygun.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntoygun.DataAcess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
